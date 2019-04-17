5c577681f44522e4b7259d026e363575fc4870e6
clintko <clint00724@gmail.com>
2019-04-17 10:54:48 -0400

    update

--- flowImg.py
+++ flowImg.py
@@ -427,7 +427,6 @@ class Transform_TSNE(BaseEstimator, TransformerMixin):
         return dat_copy
 
 #####################################################################################
-
 ### define a point type
 point_dtype = np.dtype({
     'names':   ['x', 'y'],
@@ -548,7 +547,54 @@ class Transform_Density(BaseEstimator, TransformerMixin):
         dat_copy.set_image(lst_img)
         return dat_copy
     
-#####################################################################################
+#####################################################################################        
+def leave_one_out_split(dat, k, n_samples, n_subsamples):
+    """divide the data into two parts"""
+    ### the samples index after subsampling    
+    idx_samples = np.array([[idx] * n_subsamples for idx in range(n_samples)])
+    idx_samples = idx_samples.ravel()
+
+    ### get the overall index for train and validation
+    idx_trn = np.where(idx_samples != k)[0]
+    idx_val = np.where(idx_samples == k)[0]
+    
+    ### for each label, get the index for train and validation
+    idx_trn = np.concatenate([
+        dat.which_label(label)[idx_trn] 
+        for label in dat.unique_label()
+    ])
+        
+    idx_val = np.concatenate([
+        dat.which_label(label)[idx_val] 
+        for label in dat.unique_label()
+    ])
+        
+    ### split matrix
+    mat_trn = dat.get_matrix_original()[idx_trn]
+    mat_val = dat.get_matrix_original()[idx_val]
+    
+    ### split labels
+    lab_trn = dat.label[idx_trn]
+    lab_val = dat.label[idx_val]
+        
+    ### get_coord_original()
+    n_sample = dat.get_num_sample()
+    coord = np.array([dat.get_coord(idx) for idx in range(n_sample)])
+    
+    ### split coordinates
+    coord_trn = np.concatenate(coord[idx_trn])
+    coord_val = np.concatenate(coord[idx_val])
+       
+    ### split images   
+    img_trn = dat.img[idx_trn]
+    img_val = dat.img[idx_val]
+        
+    ### wrap the train and validation data      
+    wrap_trn = Data_Wrapper(mat_trn, lab_trn, coord=coord_trn, image=img_trn)
+    wrap_val = Data_Wrapper(mat_val, lab_val, coord=coord_val, image=img_val)
+    return (wrap_trn, wrap_val)    
+
+#####################################################################################        
 def train_test_split_datawrapper(dat, n_train, n_subsample):
         """divide the data into two parts"""
         ### index for train and test
@@ -594,18 +640,11 @@ def train_test_split_datawrapper(dat, n_train, n_subsample):
             img_train = None
             img_test  = None
         
-        ### class Data_Wrapper:
-        #     A wrapper with data matrix (N, M_i, p) and data labels
-        #     matrix (N, M_i, p):      data values
-        #     label  (N,):             labels
-        #     coord  (Sum(M_i), 2):    coordination after dim. reduct. data values
-        #     img    (N, ngrid, ngrid):2D distribution from kernel smoothing of dim. reduct. 
-        #     ========================
-        #     __init__(self, matrix, label, coord = None, image = None):
         wrap_train = Data_Wrapper(mat_train, lab_train, coord=coord_train, image=img_train)
         wrap_test  = Data_Wrapper(mat_test, lab_test, coord=coord_test, image=img_test)
         return (wrap_train, wrap_test)
-                            
+    
+#####################################################################################
 class Classify_PCA_LogReg(BaseEstimator, ClassifierMixin):
     """Class: Classify_PCA_LogReg
     Args:
